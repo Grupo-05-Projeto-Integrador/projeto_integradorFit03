c1ef25dc4b54ea4ac3e1548b5a70eefdd0eca141
ArkimedesJunior <arkimedesjunior01@gmail.com>
2025-01-28 15:50:37 -0300

    Incluindo a Categoria

---
+++ src/main/java/com/fitness/aplicativofitness/repository/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.fitness.aplicativofitness.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fitness.aplicativofitness.model.Categoria;
+
+public interface CategoriaRepository  extends JpaRepository<Categoria, Long> {
+	
+   public List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
+
+}
